da2cb72f9a97cc283b9b1f68a9523694656652e7
Carlos Max <carlosmax.dev@hotmail.com>
2020-09-21 15:14:53 +0100

    change Authentication usecase to use a model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -3,5 +3,12 @@ import 'package:flutter/foundation.dart';
 import '../entities/account.dart';
 
 abstract class Authentication {
-  Future<Account> auth({@required String email, @required String password});
+  Future<Account> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String secret;
+
+  AuthenticationParams({@required this.email, @required this.secret});
 }
