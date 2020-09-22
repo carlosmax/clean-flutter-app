c7177f8d9c8dfd48c5517898afef9bb54727c48e
Carlos Max <carlosmax.dev@hotmail.com>
2020-09-22 14:17:51 +0100

    move HttpAdapter to its own file

---
+++ lib/infra/http/http_adapter.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
+
+import '../../data/http/index.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<Map> request({
+    @required String url,
+    @required String method,
+    Map body,
+  }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    final response =
+        await this.client.post(url, headers: headers, body: jsonBody);
+
+    return response.statusCode == 200 && response.body.isNotEmpty ? jsonDecode(response.body) : null;
+  }
+}
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,34 +1,9 @@
-import 'dart:convert';
-
-import 'package:faker/faker.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
+import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
+import 'package:http/http.dart';
 
-import 'package:ForDev/data/http/index.dart';
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<Map> request({
-    @required String url,
-    @required String method,
-    Map body,
-  }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json'
-    };
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    final response =
-        await this.client.post(url, headers: headers, body: jsonBody);
-
-    return response.statusCode == 200 && response.body.isNotEmpty ? jsonDecode(response.body) : null;
-  }
-}
+import 'package:ForDev/infra/http/index.dart';
 
 class ClientSpy extends Mock implements Client {}
 
