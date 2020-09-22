2608e552f05965a434be0175e2d3652ecf7fa84b
Carlos Max <carlosmax.dev@hotmail.com>
2020-09-22 20:24:16 +0100

    change statusbar to light mode

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../pages/pages.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     final primaryColor = Color.fromRGBO(136, 14, 79, 1);
     final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
     final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
@@ -12,40 +15,34 @@ class App extends StatelessWidget {
       title: '4Dev',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: primaryColor,
-        primaryColorDark: primaryColorDark,
-        primaryColorLight: primaryColorLight,
-        accentColor: primaryColor,
-        backgroundColor: Colors.white,
-        textTheme: TextTheme(
-          headline1: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: primaryColorDark
-          )
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: primaryColorLight)
+          primaryColor: primaryColor,
+          primaryColorDark: primaryColorDark,
+          primaryColorLight: primaryColorLight,
+          accentColor: primaryColor,
+          backgroundColor: Colors.white,
+          textTheme: TextTheme(
+              headline1: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: primaryColorDark)),
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: primaryColorLight)),
+            focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: primaryColor)),
+            alignLabelWithHint: true,
           ),
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: primaryColor)
-          ),
-          alignLabelWithHint: true,
-        ),
-        buttonTheme: ButtonThemeData(
-          colorScheme: ColorScheme.light(
-            primary: primaryColor,
-          ),
-          buttonColor: primaryColor,
-          splashColor: primaryColorLight,
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          textTheme: ButtonTextTheme.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18)
-          ),
-        )
-      ),
+          buttonTheme: ButtonThemeData(
+            colorScheme: ColorScheme.light(
+              primary: primaryColor,
+            ),
+            buttonColor: primaryColor,
+            splashColor: primaryColorLight,
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            textTheme: ButtonTextTheme.primary,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+          )),
       home: LoginPage(),
     );
   }
