e252027048aa17faed209d22f7e1d888415e4775
Carlos Max <carlosmax.dev@hotmail.com>
2020-09-23 22:02:16 +0100

    move classes to its own files

---
+++ lib/presentation/presenters/stream_login_presentation.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+
+import '../protocols/protocols.dart';
+
+class StreamLoginPresenter {
+  final _controller = StreamController<LoginState>.broadcast();
+  final _state = LoginState();
+  final Validation validation;
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
+
+  StreamLoginPresenter({@required this.validation});
+
+  void dispose() {
+    _controller.close();
+  }
+
+  void validateEmail(String email) {
+    _state.emailError = this.validation.validate(field: 'email', value: email);
+    _controller.add(_state);
+  }
+}
+
+class LoginState {
+  String emailError;
+}
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,39 +1,11 @@
-import 'dart:async';
-
-import 'package:faker/faker.dart';
-import 'package:flutter/material.dart';
-import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
 
-abstract class Validation {
-  String validate({@required String field, @required String value});
-}
+import 'package:ForDev/presentation/presenters/stream_login_presentation.dart';
+import 'package:ForDev/presentation/protocols/protocols.dart';
 
 class ValidationSpy extends Mock implements Validation {}
-
-class LoginState {
-  String emailError;
-}
-
-class StreamLoginPresenter {
-  final _controller = StreamController<LoginState>.broadcast();
-  final _state = LoginState();
-  final Validation validation;
-
-  Stream<String> get emailErrorStream =>
-      _controller.stream.map((state) => state.emailError);
-
-  StreamLoginPresenter({@required this.validation});
-
-  void dispose() {
-    _controller.close();
-  }
-
-  void validateEmail(String email) {
-    _state.emailError = this.validation.validate(field: 'email', value: email);
-    _controller.add(_state);
-  }
-}
 
 void main() {
   ValidationSpy validation;
